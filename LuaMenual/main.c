ab94d35de9483cce67a6ce6182e87c539154b085
王和旭 <blary@wanghexudemacbook-pro.local>
2016-04-15 23:10:01 +0800

    Initial Commit

---
+++ LuaMenual/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LuaMenual
+//
+//  Created by 王和旭 on 16/4/15.
+//  Copyright © 2016年 王和旭. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
